e5bc562821e4a4f73b7482e9bb2e78d99fb47588
luanacesar <63056960+luanacesar@users.noreply.github.com>
2021-01-20 16:45:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// const fs =require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+var superheroes = require("superheroes");
+
+var mysuper = superheroes.random();
+
+console.log(mysuper);
