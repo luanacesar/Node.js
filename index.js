97b217eb57628db077c683703da8645c3e640afb
luanacesar <luana_cesar@hotmail.com>
2021-01-21 16:01:36 -0500

    villains

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// const fs =require("fs");
-// fs.copyFileSync("file1.txt","file2.txt");
+// super heroes
+// // const fs =require("fs");
+// // fs.copyFileSync("file1.txt","file2.txt");
 
-var superheroes = require("superheroes");
+// var superheroes = require("superheroes");
 
-var mysuper = superheroes.random();
+// var mysuper = superheroes.random();
 
-console.log(mysuper);
+// console.log(mysuper);
+
+ 
+
+const supervillains = require('supervillains');
+
+
+var villains = supervillains.random();
+
+console.log(villains);
