b126b3c81619bfa7bee8eea14975511987eb4743
luanacesar <luana_cesar@hotmail.com>
2021-01-22 16:34:55 -0500

    new pages contact and about

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,6 +5,14 @@ const app = express();
 app.get("/", function(request, response){
     response.send("<h1>Hello world</h1>");
 });
+app.get("/contact", function(req,res){
+    res.send("contact me at: luana_cesar@hotmail.com");
+});
+
+app.get("/about", function(rq,re){
+    re.send("<p>Hello, my name is Luana and I am a software developer. </p>");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
