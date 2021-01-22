3645c79e27b03f1e13000198407ec2aadc7328b3
luanacesar <luana_cesar@hotmail.com>
2021-01-22 17:13:48 -0500

    nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -13,6 +13,10 @@ app.get("/about", function(rq,re){
     re.send("<p>Hello, my name is Luana and I am a software developer. </p>");
 });
 
+app.get("/location", function(request,response){
+    response.send("<p>We are located in downtown of Toronto</p>")
+})
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
