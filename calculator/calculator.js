192b817acfa59e737847b817754802407d9dd496
luanacesar <luana_cesar@hotmail.com>
2021-01-26 12:38:10 -0500

    BMI calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,6 +3,7 @@
 const express = require("express");
 // retrive data from front-end
 const bodyParser = require("body-parser");
+const { response } = require("express");
 
 const app = express();
 // urlencoded and extend
@@ -11,8 +12,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(request, response){
  response.sendFile(__dirname + "/index.html");
 });
-
-
 // to retrive and post 
 app.post("/", function(req,resp){
     //retriving 
@@ -23,6 +22,22 @@ app.post("/", function(req,resp){
     var result = num1 + num2;
 // sending/posting
     resp.send("the result is:" + result);
+});
+
+
+//BMI CALCULATOR
+
+app.get("/bmicalculator", function(requ,respo){
+    respo.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function(requ,respo){
+var weigth = requ.body.weigth;
+var heigth = requ.body.heigth;
+
+//calculate
+var total =  weigth + heigth;
+respo.send("Your BMI is" + total);
 });
 
 app.listen(3000, function()
